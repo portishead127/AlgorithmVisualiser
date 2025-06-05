9a82f1aea1e37d195e4711e36dbf1c37a281cd9b
Aaron Portis <aaronjportis@gmail.com>
2025-06-05 15:50:56 +0100

    Attempting to add sounds
    Unsuccessfully

--- AlgorithmVisualiser/InsertionSort.cs
+++ AlgorithmVisualiser/InsertionSort.cs
@@ -17,9 +17,33 @@ namespace AlgorithmVisualiser
 
         public async void Sort(int[] array)
         {
-            for(int i = 1; i >=0; i--)
+            for (int i = 1; i <= array.Length; i++)
             {
+                int j = i - 1;
+                while (j >= 0 && j != array.Length - 1)
+                {
+                    if (array[j] > array[j+1])
+                    {
+                        await visualiser.Next(array, new int[] { j }, new int[] { i }, Array.Empty<int>());
+                        // Swap elements
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        // Highlight the swapped indices
+                    }
+                    j--;
+                }
+            }
+            CompletionAnimation(array);
+        }
 
+        public async void CompletionAnimation(int[] array)
+        {
+            await visualiser.Next(array, null, null, null);
+
+            for (int i = 1; i <= array.Length; i++)
+            {
+                await visualiser.Next(array, new int[0], new int[0], Enumerable.Range(0, i).ToArray());
             }
         }
     }
