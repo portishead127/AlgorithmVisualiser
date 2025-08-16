06cbcac0f25d3ecae5ad9454ad5425a3e54240de
Aaron Portis <aaronjportis@gmail.com>
2025-08-16 14:40:22 +0100

    Attempting to seperate the drawing of the frame from the sorting itself

--- AlgorithmVisualiser/SortingAlgorithm.cs
+++ AlgorithmVisualiser/SortingAlgorithm.cs
@@ -6,8 +6,28 @@ using System.Threading.Tasks;
 
 namespace AlgorithmVisualiser
 {
-    internal interface SortingAlgorithm
+    public abstract class SortingAlgorithm
     {
-        void Sort(int[] array);
+        SortingAlgorithmVisualiser visualiser;
+        public int[] array;
+        private int[] redIndices;
+        private int[] greenIndices;
+        private int[] blueIndices;
+
+        public int[] Array {
+            get
+            {
+                return array;
+            }
+            set
+            {
+                if(Array != null)
+                {
+                    array = value;
+                }
+            }
+        }
+
+        public abstract int[] Sort(int[] array);
     }
 }
--- AlgorithmVisualiser/SortingAlgorithmVisualiser.cs
+++ AlgorithmVisualiser/SortingAlgorithmVisualiser.cs
@@ -15,18 +15,11 @@ namespace AlgorithmVisualiser
     public partial class SortingAlgorithmVisualiser : Form
     {
         SortingAlgorithm sortingAlgorithm;
-        int[] currentArray;
-        readonly int[] originalArray;
-        int[] redHighlightIndices;
-        int[] greenHighlightIndices;
-        int[] blueHighlightIndices;
 
-        public SortingAlgorithmVisualiser(int[] array)
+        public SortingAlgorithmVisualiser(SortingAlgorithm sortingAlgorithm)
         {
-            sortingAlgorithm = new BubbleSort(this); // You can change this to any sorting algorithm you implement
+            this.sortingAlgorithm = sortingAlgorithm; // You can change this to any sorting algorithm you implement
             InitializeComponent();
-            currentArray = array;
-            originalArray = (int[])array.Clone(); // Store the original array for reference
             pnlRect.Paint += pnlRect_Paint;
         }
 
@@ -92,54 +85,54 @@ namespace AlgorithmVisualiser
             button1.Enabled = false; // Disable the button to prevent multiple clicks
         }
 
-        public static void PlayBeep(UInt16 frequency, int msDuration = 100, UInt16 volume = 16383)
-        {
-            var mStrm = new MemoryStream();
-            BinaryWriter writer = new BinaryWriter(mStrm);
+        //public static void PlayBeep(UInt16 frequency, int msDuration = 100, UInt16 volume = 16383)
+        //{
+        //    var mStrm = new MemoryStream();
+        //    BinaryWriter writer = new BinaryWriter(mStrm);
 
-            const double TAU = 2 * Math.PI;
-            int formatChunkSize = 16;
-            int headerSize = 8;
-            short formatType = 1;
-            short tracks = 1;
-            int samplesPerSecond = 44100;
-            short bitsPerSample = 16;
-            short frameSize = (short)(tracks * ((bitsPerSample + 7) / 8));
-            int bytesPerSecond = samplesPerSecond * frameSize;
-            int waveSize = 4;
-            int samples = (int)((decimal)samplesPerSecond * msDuration / 1000);
-            int dataChunkSize = samples * frameSize;
-            int fileSize = waveSize + headerSize + formatChunkSize + headerSize + dataChunkSize;
-            // var encoding = new System.Text.UTF8Encoding();
-            writer.Write(0x46464952); // = encoding.GetBytes("RIFF")
-            writer.Write(fileSize);
-            writer.Write(0x45564157); // = encoding.GetBytes("WAVE")
-            writer.Write(0x20746D66); // = encoding.GetBytes("fmt ")
-            writer.Write(formatChunkSize);
-            writer.Write(formatType);
-            writer.Write(tracks);
-            writer.Write(samplesPerSecond);
-            writer.Write(bytesPerSecond);
-            writer.Write(frameSize);
-            writer.Write(bitsPerSample);
-            writer.Write(0x61746164); // = encoding.GetBytes("data")
-            writer.Write(dataChunkSize);
-            {
-                double theta = frequency * TAU / (double)samplesPerSecond;
-                // 'volume' is UInt16 with range 0 thru Uint16.MaxValue ( = 65 535)
-                // we need 'amp' to have the range of 0 thru Int16.MaxValue ( = 32 767)
-                double amp = volume >> 2; // so we simply set amp = volume / 2
-                for (int step = 0; step < samples; step++)
-                {
-                    short s = (short)(amp * Math.Sin(theta * (double)step));
-                    writer.Write(s);
-                }
-            }
+        //    const double TAU = 2 * Math.PI;
+        //    int formatChunkSize = 16;
+        //    int headerSize = 8;
+        //    short formatType = 1;
+        //    short tracks = 1;
+        //    int samplesPerSecond = 44100;
+        //    short bitsPerSample = 16;
+        //    short frameSize = (short)(tracks * ((bitsPerSample + 7) / 8));
+        //    int bytesPerSecond = samplesPerSecond * frameSize;
+        //    int waveSize = 4;
+        //    int samples = (int)((decimal)samplesPerSecond * msDuration / 1000);
+        //    int dataChunkSize = samples * frameSize;
+        //    int fileSize = waveSize + headerSize + formatChunkSize + headerSize + dataChunkSize;
+        //    // var encoding = new System.Text.UTF8Encoding();
+        //    writer.Write(0x46464952); // = encoding.GetBytes("RIFF")
+        //    writer.Write(fileSize);
+        //    writer.Write(0x45564157); // = encoding.GetBytes("WAVE")
+        //    writer.Write(0x20746D66); // = encoding.GetBytes("fmt ")
+        //    writer.Write(formatChunkSize);
+        //    writer.Write(formatType);
+        //    writer.Write(tracks);
+        //    writer.Write(samplesPerSecond);
+        //    writer.Write(bytesPerSecond);
+        //    writer.Write(frameSize);
+        //    writer.Write(bitsPerSample);
+        //    writer.Write(0x61746164); // = encoding.GetBytes("data")
+        //    writer.Write(dataChunkSize);
+        //    {
+        //        double theta = frequency * TAU / (double)samplesPerSecond;
+        //        // 'volume' is UInt16 with range 0 thru Uint16.MaxValue ( = 65 535)
+        //        // we need 'amp' to have the range of 0 thru Int16.MaxValue ( = 32 767)
+        //        double amp = volume >> 2; // so we simply set amp = volume / 2
+        //        for (int step = 0; step < samples; step++)
+        //        {
+        //            short s = (short)(amp * Math.Sin(theta * (double)step));
+        //            writer.Write(s);
+        //        }
+        //    }
 
-            mStrm.Seek(0, SeekOrigin.Begin);
-            new System.Media.SoundPlayer(mStrm).PlaySync();
-            writer.Close();
-            mStrm.Close();
-        }
+        //    mStrm.Seek(0, SeekOrigin.Begin);
+        //    new System.Media.SoundPlayer(mStrm).PlaySync();
+        //    writer.Close();
+        //    mStrm.Close();
+        //}
     }
 }
